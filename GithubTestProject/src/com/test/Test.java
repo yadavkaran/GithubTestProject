248f886dd323d3a2ee0dbb49c412e1dab5bbaa2b
Karan <karan.yadav@neosoftmail.com>
2021-04-23 11:24:48 +0530

    First Commit

---
+++ GithubTestProject/src/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Test {
+	public static void main(String[] args) {
+		String code = "SE";
+		if (code == "SE")
+			System.out.println("Software Engineer");
+		else
+			System.out.println("Not a software Engineer");
+	}
+}
